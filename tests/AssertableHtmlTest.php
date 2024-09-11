e224060cc38c3e533d6fa6531d44ddfcd3655a85
Jamie York <ziadoz@gmail.com>
2024-09-11 22:27:54 +0100

    wip (test get document)

--- tests/AssertableHtmlTest.php
+++ tests/AssertableHtmlTest.php
@@ -96,6 +96,20 @@ class AssertableHtmlTest extends TestCase
         });
     }
 
+    public function testGetDocument(): void
+    {
+        $assertable = new AssertableHtml($document = $this->getFixtureHtml('skeleton.html'), 'body');
+        $this->assertSame($document, $assertable->getDocument());
+
+        $assertable->with('ul.inner', function (AssertableHtml $assertableInner) use ($document): void {
+            $this->assertSame($document, $assertableInner->getDocument());
+
+            $assertableInner->elsewhere('ul.outer', function (AssertableHtml $assertableElsewhere) use ($document): void {
+                $this->assertSame($document, $assertableElsewhere->getDocument());
+            });
+        });
+    }
+
     public function testGetDocumentHtml(): void
     {
         $assertable = new AssertableHtml($this->getFixtureHtml('skeleton.html'), 'body');
