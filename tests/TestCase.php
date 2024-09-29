2e798f0d09ced2f3b926d8dc43a5835462cbf9bc
Jamie York <ziadoz@gmail.com>
2024-09-29 15:43:21 +0100

    wip (1l fixture)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,15 +6,9 @@ use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 class TestCase extends PHPUnitTestCase
 {
-    /** Get the contents of a fixture file. */
-    public function getFixture(string $file): string
-    {
-        return file_get_contents(__DIR__ . '/Fixtures/' . $file);
-    }
-
     /** Get the contents of a fixture file as an HTML document. */
     public function getFixtureHtml(string $file): HtmlDocument
     {
-        return HtmlDocument::createFromString($this->getFixture($file));
+        return HtmlDocument::createFromString(file_get_contents(__DIR__ . '/Fixtures/' . $file));
     }
 }
