73bfec1a7db2d273a0fc7a3a8ab0337fb9b2e879
Jamie York <ziadoz@gmail.com>
2024-09-29 15:40:31 +0100

    wip (1l docblocks)

--- src/AssertableHtml.php
+++ src/AssertableHtml.php
@@ -7,28 +7,20 @@ use PHPUnit\Framework\Assert as PHPUnit;
 
 class AssertableHtml
 {
-    /**
-     * The root HTML document or element to perform assertions on.
-     */
+    /** The root HTML document or element to perform assertions on. */
     protected HtmlDocument|HtmlElement $root;
 
-    /**
-     * The selector to identify the root element.
-     */
+    /** The selector to identify the root element. */
     protected string $selector;
 
-    /**
-     * Create an assertable HTML instance.
-     */
+    /** Create an assertable HTML instance. */
     public function __construct(HtmlDocument|HtmlElement $document, string $selector)
     {
         $this->root = $this->determineRoot($document, $selector);
         $this->selector = $selector;
     }
 
-    /**
-     * Determine the root element to perform assertions on. The root can only ever be a single element.
-     */
+    /** Determine the root element to perform assertions on. The root can only ever be a single element. */
     protected function determineRoot(HtmlDocument|HtmlElement $document, string $selector): HtmlElement
     {
         $nodes = $document->querySelectorAll($selector);
@@ -42,9 +34,7 @@ class AssertableHtml
         return $nodes[0];
     }
 
-    /**
-     * Performs assertions on a scoped selection.
-     */
+    /** Performs assertions on a scoped selection. */
     public function with(string $selector, ?callable $callback = null, bool $prepend = true): static
     {
         $instance = new static($this->getDocument(), ($prepend ? $this->selector . ' ' . $selector : $selector));
@@ -56,65 +46,49 @@ class AssertableHtml
         return $instance;
     }
 
-    /**
-     * Perform assertions outside the scoped selection.
-     */
+    /** Perform assertions outside the scoped selection. */
     public function elsewhere(string $selector, ?callable $callback = null): static
     {
         return $this->with($selector, $callback, false);
     }
 
-    /**
-     * Return the underlying HTML document instance.
-     */
+    /** Return the underlying HTML document instance. */
     public function getDocument(): HtmlDocument
     {
         return $this->root instanceof HtmlElement ? $this->root->ownerDocument : $this->root;
     }
 
-    /**
-     * Return the root HTML document or element assertions are being performed on.
-     */
+    /** Return the root HTML document or element assertions are being performed on. */
     public function getRoot(): HtmlDocument|HtmlElement
     {
         return $this->root;
     }
 
-    /**
-     * Return the selector used to find the root element.
-     */
+    /** Return the selector used to find the root element. */
     public function getSelector(): string
     {
         return $this->selector;
     }
 
-    /**
-     * Return the document HTML.
-     */
+    /** Return the document HTML. */
     public function getDocumentHtml(): string
     {
         return $this->getDocument()->saveHtml();
     }
 
-    /**
-     * Return the root element HTML.
-     */
+    /** Return the root element HTML. */
     public function getRootHtml(): string
     {
         return $this->getDocument()->saveHtml($this->getRoot());
     }
 
-    /**
-     * Dump the root element HTML.
-     */
+    /** Dump the root element HTML. */
     public function dump(): void
     {
         dump($this->getRootHtml());
     }
 
-    /**
-     * Dump and die the root element HTML.
-     */
+    /** Dump and die the root element HTML. */
     public function dd(): never
     {
         dd($this->getRootHtml());
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,17 +6,13 @@ use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 class TestCase extends PHPUnitTestCase
 {
-    /**
-     * Get the contents of a fixture file.
-     */
+    /** Get the contents of a fixture file. */
     public function getFixture(string $file): string
     {
         return file_get_contents(__DIR__ . '/Fixtures/' . $file);
     }
 
-    /**
-     * Get the contents of a fixture file as an HTML document.
-     */
+    /** Get the contents of a fixture file as an HTML document. */
     public function getFixtureHtml(string $file): HtmlDocument
     {
         return HtmlDocument::createFromString($this->getFixture($file));
