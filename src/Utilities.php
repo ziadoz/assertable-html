09a1f3393e29bd7b11999b46a313205ff1b9201b
Jamie York <ziadoz@gmail.com>
2024-12-24 16:16:43 +0000

    wip (one line docblock)

--- src/Utilities.php
+++ src/Utilities.php
@@ -26,11 +26,7 @@ class Utilities
         return implode('', $parts);
     }
 
-    /**
-     * Normalise the whitespace of the given string.
-     *
-     * @link: https://github.com/symfony/symfony/pull/48940
-     */
+    /** Normalise the whitespace of the given string. @link: https://github.com/symfony/symfony/pull/48940 */
     public static function normaliseWhitespace(string $string): string
     {
         return trim(preg_replace("/[ \n\r\t\x0C]{2,}+|[\n\r\t\x0C]/", ' ', $string), " \n\r\t\x0C");
