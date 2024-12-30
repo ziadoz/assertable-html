1af3060213b7669d9f0ed965473ea05dfa906f02
Jamie York <ziadoz@gmail.com>
2024-12-30 19:15:03 +0000

    wip (integration tests basics)

---
+++ tests/Integration/IntegrationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ziadoz\AssertableHtml\Tests\Integration;
+
+use Ziadoz\AssertableHtml\Tests\TestCase;
+
+class IntegrationTest extends TestCase
+{
+    public function test_it(): void
+    {
+        $this->markTestSkipped('@todo');
+    }
+}
