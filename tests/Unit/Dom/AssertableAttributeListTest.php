8af880b3d54917be4a40d02ad40ae80ddce9eb92
Jamie York <ziadoz@gmail.com>
2025-01-20 22:06:19 +0000

    wip (test assert attrs)

---
+++ tests/Unit/Dom/AssertableAttributeListTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziadoz\AssertableHtml\Tests\Unit\Dom;
+
+use PHPUnit\Framework\TestCase;
+use Ziadoz\AssertableHtml\Dom\AssertableDocument;
+
+class AssertableAttributeListTest extends TestCase
+{
+    public function test_attribute_list(): void
+    {
+        $assertable = AssertableDocument::createFromString('<p class="foo" id="bar" data-bar="  foo-bar  " aria-label="foo">Foo</p>', LIBXML_NOERROR)
+            ->querySelector('p')
+            ->attributes;
+
+        // Value
+        $this->assertSame('foo', $assertable->value('class'));
+        $this->assertSame('bar', $assertable->value('id'));
+        $this->assertSame('  foo-bar  ', $assertable->value('data-bar'));
+        $this->assertSame('foo-bar', $assertable->value('data-bar', true));
+        $this->assertSame('foo', $assertable->value('aria-label'));
+
+        // Empty
+        $this->assertFalse($assertable->empty());
+
+        // Names
+        $this->assertSame(['class', 'id', 'data-bar', 'aria-label'], $assertable->names());
+
+        // Has
+        $this->assertTrue($assertable->has('class'));
+        $this->assertTrue($assertable->has('id'));
+        $this->assertTrue($assertable->has('data-bar'));
+        $this->assertTrue($assertable->has('aria-label'));
+        $this->assertFalse($assertable->has('foo-bar'));
+
+        // Array Access
+        $this->assertTrue(isset($assertable['class']));
+        $this->assertSame('foo', $assertable['class']);
+        $this->assertTrue(isset($assertable['id']));
+        $this->assertSame('bar', $assertable['id']);
+        $this->assertTrue(isset($assertable['data-bar']));
+        $this->assertSame('  foo-bar  ', $assertable['data-bar']);
+        $this->assertTrue(isset($assertable['aria-label']));
+        $this->assertSame('foo', $assertable['aria-label']);
+        $this->assertNull($assertable['foo-bar-baz'] ?? null);
+
+        // Count
+        $this->assertCount(4, $assertable);
+
+        // Stringable
+        $this->assertSame('class="foo" id="bar" data-bar="  foo-bar  " aria-label="foo"', $assertable->__toString());
+
+        // Array
+        $this->assertSame([
+            'class'      => 'foo',
+            'id'         => 'bar',
+            'data-bar'   => '  foo-bar  ',
+            'aria-label' => 'foo',
+        ], $assertable->toArray());
+    }
+}
