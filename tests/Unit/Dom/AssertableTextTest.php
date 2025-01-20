36bfae04ae9935632144e4c29e502197520595b4
Jamie York <ziadoz@gmail.com>
2025-01-20 20:17:54 +0000

    wip (test assertable text)

---
+++ tests/Unit/Dom/AssertableTextTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziadoz\AssertableHtml\Tests\Unit\Dom;
+
+use PHPUnit\Framework\TestCase;
+use Ziadoz\AssertableHtml\Dom\AssertableDocument;
+
+class AssertableTextTest extends TestCase
+{
+    public function test_text(): void
+    {
+        $assertable = AssertableDocument::createFromString('<p>  <strong>Foo</strong>  Bar  </p>', LIBXML_NOERROR)
+            ->querySelector('p')
+            ->text;
+
+        $this->assertSame('  Foo  Bar  ', $assertable->__toString());
+
+        $this->assertSame('  Foo  Bar  ', $assertable->value());
+        $this->assertSame('Foo Bar', $assertable->value(true));
+
+        $this->assertTrue($assertable->startsWith('  Foo'));
+        $this->assertTrue($assertable->startsWith('Foo', true));
+        $this->assertFalse($assertable->startsWith('Qux'));
+        $this->assertFalse($assertable->startsWith('Qux', true));
+
+        $this->assertTrue($assertable->endsWith('Bar  '));
+        $this->assertTrue($assertable->endsWith('Bar', true));
+        $this->assertFalse($assertable->endsWith('Qux'));
+        $this->assertFalse($assertable->endsWith('Qux', true));
+
+        $this->assertTrue($assertable->contains('o  B'));
+        $this->assertTrue($assertable->contains('o B', true));
+        $this->assertFalse($assertable->contains('Qux'));
+        $this->assertFalse($assertable->contains('Qux', true));
+    }
+}
