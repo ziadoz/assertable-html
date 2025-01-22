15c59878cca6f3724ee23d9644556e22e00462fc
Jamie York <ziadoz@gmail.com>
2025-01-22 19:46:08 +0000

    wip (lint)

--- src/Concerns/IdentifiesElement.php
+++ src/Concerns/IdentifiesElement.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Ziadoz\AssertableHtml\Concerns;
 
 use Dom\Element;
-use Dom\HTMLElement;
 use Ziadoz\AssertableHtml\Support\Whitespace;
 
 trait IdentifiesElement
