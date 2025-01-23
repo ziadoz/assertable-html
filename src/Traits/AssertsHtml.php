d41f5f9d28592200c2afc7dd76bb1d70f79b79ad
Jamie York <ziadoz@gmail.com>
2025-01-23 22:20:22 +0000

    wip (assert doc)

--- src/Traits/AssertsHtml.php
+++ src/Traits/AssertsHtml.php
@@ -9,7 +9,7 @@ use Ziadoz\AssertableHtml\Dom\AssertableDocument;
 trait AssertsHtml
 {
     /** Return a configured assertable HTML document. */
-    protected function assertable(string $html, int $options = 0, ?string $overrideEncoding = null): AssertableDocument
+    public function assertableDocument(string $html, int $options = 0, ?string $overrideEncoding = null): AssertableDocument
     {
         return is_file($html)
             ? AssertableDocument::createFromFile($html, $options, $overrideEncoding)
@@ -19,18 +19,18 @@ trait AssertsHtml
     /** Return an assertable HTML document. */
     public function assertHtml(string $html, callable $callback, int $options = 0, ?string $overrideEncoding = null): void
     {
-        $this->assertable($html, $options, $overrideEncoding)->scope($callback);
+        $this->assertableDocument($html, $options, $overrideEncoding)->scope($callback);
     }
 
     /** Return an assertable HTML document scoped to <head>. */
     public function assertHead(string $html, callable $callback, int $options = 0, ?string $overrideEncoding = null): void
     {
-        $this->assertable($html, $options, $overrideEncoding)->with('head', $callback);
+        $this->assertableDocument($html, $options, $overrideEncoding)->with('head', $callback);
     }
 
     /** Return an assertable HTML document scoped to <body>. */
     public function assertBody(string $html, callable $callback, int $options = 0, ?string $overrideEncoding = null): void
     {
-        $this->assertable($html, $options, $overrideEncoding)->with('body', $callback);
+        $this->assertableDocument($html, $options, $overrideEncoding)->with('body', $callback);
     }
 }
