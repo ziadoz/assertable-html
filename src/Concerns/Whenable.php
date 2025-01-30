637f6c8e9ff8164bf888d85773f5211b73c16feb
Jamie <ziadoz@gmail.com>
2025-01-30 11:16:58 +0000

    wip (when truthy param not optional)

--- src/Concerns/Whenable.php
+++ src/Concerns/Whenable.php
@@ -10,10 +10,10 @@ trait Whenable
      * Return a value if the given condition is true, otherwise return a default.
      *
      * @param  callable(static $assertable): bool|bool  $condition
-     * @param  ?callable(static $assertable): ?callable  $callback
+     * @param  callable(static $assertable): callable  $callback
      * @param  ?callable(static $assertable): ?callable  $default
      */
-    public function when(callable|bool $condition, ?callable $callback = null, ?callable $default = null): static
+    public function when(callable|bool $condition, callable $callback, ?callable $default = null): static
     {
         $condition = is_callable($condition) ? $condition($this) : $condition;
 
