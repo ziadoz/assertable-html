bcadc9c9effefd74606b223bebcf12a7879b6e6d
Jamie York <ziadoz@gmail.com>
2025-06-25 18:23:54 +0100

    assert method wip

--- src/Dom/Elements/AssertableForm.php
+++ src/Dom/Elements/AssertableForm.php
@@ -6,13 +6,95 @@ namespace Ziadoz\AssertableHtml\Dom\Elements;
 
 use Dom\Element;
 use Dom\HTMLElement;
+use PHPUnit\Framework\Assert as PHPUnit;
 use Ziadoz\AssertableHtml\Contracts\PromotableAssertableElement;
 use Ziadoz\AssertableHtml\Dom\AssertableElement;
 
 readonly class AssertableForm extends AssertableElement implements PromotableAssertableElement
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Interface
+    |--------------------------------------------------------------------------
+    */
+
+    /** {@inheritDoc} */
     public static function shouldPromote(Element|HTMLElement $element): bool
     {
         return $element->tagName === 'FORM';
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Assert Method
+    |--------------------------------------------------------------------------
+    */
+
+    /** Assert the form has the given method attribute (GET or POST). */
+    public function assertMethod(string $method, ?string $message = null): static
+    {
+        $method = $this->formatMethod($method);
+
+        $this->isValidMethod($method);
+
+        $this->assertAttribute(
+            'method',
+            fn (?string $value): bool => trim(mb_strtolower((string) $value)) === $method,
+            $message ?? sprintf(
+                "The form method doesn't equal [%s].",
+                $method,
+            ),
+        );
+
+        return $this;
+    }
+
+    /** Assert the form has the given hidden input method (PUT, PATCH or DELETE). */
+    public function assertHiddenInputMethod(string $selector, string $method, ?string $message = null): static
+    {
+        $method = $this->formatMethod($method);
+
+        $this->isValidHiddenInputMethod($method);
+
+        $this->querySelector($selector)
+            ->assertMatchesSelector('input[type="hidden"]')
+            ->assertAttribute(
+                'value',
+                fn (?string $value): bool => trim(mb_strtolower((string) $value)) === $method,
+                $message ?? sprintf(
+                    "The form hidden input method doesn't equal [%s].",
+                    $method,
+                ),
+            );
+
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Internal
+    |--------------------------------------------------------------------------
+    */
+
+    /** Lowercase and trim the given method. */
+    protected function formatMethod(string $method): string
+    {
+        return trim(mb_strtolower($method));
+    }
+
+    /** Fail if the method isn't a valid form method. */
+    protected function isValidMethod(string $method): void
+    {
+        if (! in_array($this->formatMethod($method), ['get', 'post', 'dialog'])) {
+            PHPUnit::fail(sprintf("The method [%s] isn't a valid form method.", $method));
+        }
+    }
+
+    /** Fail if the method isn't a valid hidden input method. */
+    protected function isValidHiddenInputMethod(string $method): void
+    {
+        if (! in_array($this->formatMethod($method), ['put', 'patch', 'delete'])) {
+            PHPUnit::fail(sprintf("The method [%s] isn't a valid form method.", $method));
+        }
     }
 }
--- tests/Integration/AssertableFormTest.php
+++ tests/Integration/AssertableFormTest.php
@@ -6,24 +6,31 @@ namespace Ziadoz\AssertableHtml\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
 use Ziadoz\AssertableHtml\Dom\AssertableDocument;
-use Ziadoz\AssertableHtml\Dom\Elements\AssertableForm;
 
 class AssertableFormTest extends TestCase
 {
     public function test_assertable_form(): void
     {
-        $this->markTestSkipped('@todo');
+        // Form Methods
+        AssertableDocument::createFromString('<form method="get"></form>', LIBXML_HTML_NOIMPLIED)
+            ->querySelector('form')
+            ->assertMethod('get');
 
-        $html = AssertableDocument::createFromString(<<<'HTML'
-        <form method="get" action="/foo/bar" enctype="multipart/form-data">
-            <label>Name <input type="text" name="name" value="Foo Bar"></label>
-            <label>Age <input type="number" name="age" value="42"></label>
-            <button type="submit">Save</button>
-        </form>
-        HTML, LIBXML_HTML_NOIMPLIED);
-
-        $html->one('form', function (AssertableForm $form) {
-            $form->dump();
-        });
+        AssertableDocument::createFromString('<form><input type="hidden" name="_method" value="put"></form>', LIBXML_HTML_NOIMPLIED)
+            ->querySelector('form')
+            ->assertHiddenInputMethod('input[name="_method"]', 'put');
     }
+
+    // <form method="get" action="/foo/bar" enctype="multipart/form-data" id="foo" class="bar">
+    // <!-- Inputs -->
+    // <label>Name <input type="text" name="name" value="Foo Bar"></label>
+    // <label>Age <input type="number" name="age" value="42"></label>
+    //
+    // <!-- Upload Inputs -->
+    // <label>File <input type="file" name="file"></label>
+    //
+    // <!-- Submits -->
+    // <button type="submit">Save</button>
+    // </form>
+
 }
